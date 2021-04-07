7957a7f650a8ce696444d3fc7b10cab9e1997a93
fuurin <komatsuemgbr0624@gmail.com>
2021-04-07 10:17:01 +0900

    no auto update

--- src/Updater.svelte
+++ src/Updater.svelte
@@ -1,26 +1,8 @@
 <script>
   import { Button } from 'sveltestrap'
   import { createEventDispatcher } from 'svelte'
-
   const dispatch = createEventDispatcher()
-  const UPDATE_INTERVAL = 5000 // ms
-  let timer = null
-
   const update = () => dispatch('update')
-  const autoUpdate = (checked) => {
-    if (checked) {
-      update()
-      timer = setInterval(update, UPDATE_INTERVAL)
-    } else if (timer !== null) {
-      clearInterval(timer)
-    }
-  }
 </script>
 
-<Button color='secondary' on:click={update}>
-  更新
-</Button>
-<label class='ml-3'>
-  <input type="checkbox" on:change={e => autoUpdate(e.target.checked)}/>
-  自動更新
-</label>
+<Button color='secondary' on:click={update}>更新</Button>
