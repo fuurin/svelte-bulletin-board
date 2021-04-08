4c815f83d8c9384b869aeca416c5c6b7c7d75b86
fuurin <komatsuemgbr0624@gmail.com>
2021-04-08 10:40:41 +0900

    Updater fix conflicts

--- src/Updater.svelte
+++ src/Updater.svelte
@@ -1,33 +1,8 @@
 <script>
   import { Button } from 'sveltestrap'
-<<<<<<< HEAD
-  import { updateComments } from './stores'
-
-  const UPDATE_INTERVAL = 5000 // ms
-  let timer = null
-
-  const autoUpdate = (checked) => {
-    if (checked) {
-      updateComments()
-      timer = setInterval(updateComments, UPDATE_INTERVAL)
-    } else if (timer !== null) {
-      clearInterval(timer)
-    }
-  }
-</script>
-
-<Button color='secondary' on:click={updateComments}>
-  更新
-</Button>
-<label class='ml-3'>
-  <input type="checkbox" on:change={e => autoUpdate(e.target.checked)}/>
-  自動更新
-</label>
-=======
   import { createEventDispatcher } from 'svelte'
   const dispatch = createEventDispatcher()
   const update = () => dispatch('update')
 </script>
 
 <Button color='secondary' on:click={update}>更新</Button>
->>>>>>> master
